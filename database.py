ef3684c345e86383a4cf68bb70b403993be3ab09
Arunabh Das <arunabhdas@gmail.com>
2022-01-08 17:47:18 -0500

    Added get_all_todos function

---
+++ database.py
@@ -0,0 +1,38 @@
+import sqlite3
+from typing import List
+import datetime
+from model import Todo
+
+conn = sqlite3.connect('todos.db')
+c = conn.cursor()
+
+def create_table():
+    c.execute(""""CREATE TABLE IF NOT EXISTS todos (
+        task text,
+        category text,
+        date_added text,
+        date_completed text,
+        status integer,
+        position integer
+    )""")
+
+create_table()
+
+def insert_todo(todo: Todo):
+    c.execute('select count(*) FROM todos')
+    count = c.fetchone()[0]
+    todo.position = count if count else 0
+    with conn:
+        c.execute('INSERT INTO todos VALUES (:task, :category, :date_added, :date_completed, :status, :position)',
+        {'task': todo.task, 'category': todo.category, 'date_added': todo.date_added,
+        'date_completed': todo.date_completed, 'status': todo.status, 'position': todo.position  })
+
+def get_all_todos() -> List(Todo):
+    c.execute('select * from todos')
+    results = c.fetchall()
+    todos = []
+    for result in results:
+        todos.append(Todo(*results))
+    return todos
+
+    
--- model.py
+++ model.py
@@ -8,3 +8,6 @@ class Todo:
         self.date_completed = date_completed if date_completed is not None else None
         self.status = status if status is not None else 1 # 1 = open, 2 = completed
         self.position = position if position is not None else None
+
+    def __repr__(self) -> str:
+        return f"({self.task}, {self.category}, {self.date_added}, {self.date_completed}, {self.status}, {self.position}"
